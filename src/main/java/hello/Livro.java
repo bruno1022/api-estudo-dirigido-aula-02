fc905e7084b98d16328dad96596f1a3289427aab
bruno1022 <40252090+bruno1022@users.noreply.github.com>
2018-06-13 22:28:46 -0300

    instanciando livro

---
+++ src/main/java/hello/Livro.java
@@ -0,0 +1,37 @@
+package hello;
+
+public class Livro {
+	
+	private long id;
+	private String titulo;
+	private String autor;
+	private String isbn;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getAutor() {
+		return autor;
+	}
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	
+	
+
+}
