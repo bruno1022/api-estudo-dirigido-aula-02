0b524fd8e48bc9369194f06e1f22134472293cbf
Bruno Erreira de Almeida <brunof@bhzsist30.BHZ>
2018-06-14 12:40:19 -0300

    instaciando objetos

---
+++ src/main/java/entity/Comentario.java
@@ -0,0 +1,28 @@
+package entity;
+
+public class Comentario {
+	
+	private Long id;
+	private String isbn;
+	private String comentario;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public String getComentario() {
+		return comentario;
+	}
+	public void setComentario(String comentario) {
+		this.comentario = comentario;
+	}
+
+}
